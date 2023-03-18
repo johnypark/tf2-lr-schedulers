3552a16bcd7fc2eac597662f0a5b165662f94ffd
johnypark <parkjohnyc@gmail.com>
2023-03-17 20:27:59 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # tf2-lr-schedulers
  Modern LearningRateSchedulers in Tensorflow2
+
+
+![LRS_comparison](images/Different_LR_Schedulers.png)
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tf2-lr-schedulers", # Replace with your own username
+    version="0.0.1",
+    author="John Park",
+    author_email="",
+    description="TensorFlow2 LR Schedulers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/johnypark/tf2-lr-schedulers",
+    packages=setuptools.find_packages(),
+    install_requires = ['tensorflow',
+                        'numpy',
+                        'typeguard'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD3 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+

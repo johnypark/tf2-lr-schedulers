c74bafa739d9334522ab5f1422a60e7f0d7ba8fc
johnypark <parkjohnyc@gmail.com>
2023-03-19 14:08:16 -0400

    Fix "    TypeError: list indices must be integers or slices, not Tensor"

--- tf2_lr_schedulers/Basis.py
+++ tf2_lr_schedulers/Basis.py
@@ -7,8 +7,8 @@ def constant_func(learning_rate, step):
     output = tf.ones(step.shape)*learning_rate
     return output
 
-def cosine_annealing_func(step, start, end):#, step_size_partial, cycle):
-    x = step # / step_size_partial
+def cosine_annealing_func(step, start, end):
+    x = step 
     cosine_annealing = 1 + tf.math.cos(tf.constant(np.pi) * x)
     return end + 0.5 * (start - end) * cosine_annealing
 
@@ -23,7 +23,7 @@ class ComposeLR(tf.keras.optimizers.schedules.LearningRateSchedule):
     def __init__(
         self,
         cycle_size,
-        shift_points, 
+        interval_fractions, 
         list_funcs, 
         initial_learning_rate = 0.1,
         scale_mode="cycle",
@@ -35,7 +35,7 @@ class ComposeLR(tf.keras.optimizers.schedules.LearningRateSchedule):
 
         Args:
             cycle_size (_type_): _description_
-            shift_points (list): # list of shifting points, check if sum(shift_points)< =1, otherwise need rescaling. 
+            interval_fractions (list): # list of shifting points, check if sum(interval_fractions)< =1, otherwise need rescaling. 
             list_funcs (list), # list of functions
             initial_learning_rate (float, optional): _description_. Defaults to 0.1.
             final_lr_scale (float, optional): _description_. Defaults to 1.0.
@@ -47,11 +47,11 @@ class ComposeLR(tf.keras.optimizers.schedules.LearningRateSchedule):
         self.cycle_size = cycle_size
         self.scale_fn = scale_fn
         self.scale_mode = scale_mode
-        self.shift_points = shift_points
+        self.interval_fractions = interval_fractions
         self.final_lr_scale = final_lr_scale
         self.name = name
         self._total_steps = cycle_size
-        self._shift_steps = [ele*self._total_steps for ele in self.shift_points]
+        self._interval_steps = [ele*self._total_steps for ele in self.interval_fractions]
         self.list_funcs = list_funcs
 
     def __call__(self, step, optimizer=False):
@@ -64,20 +64,18 @@ class ComposeLR(tf.keras.optimizers.schedules.LearningRateSchedule):
             lambda: tf.expand_dims(step, axis = 0),
             lambda: step)
             total_steps = tf.cast(self._total_steps, dtype)
-            print(self._shift_steps)
-            shift_steps = [tf.cast(ele, dtype) for ele in self._shift_steps] 
-            # Check in progress on the cycle
+            shift_steps = [tf.cast(ele, dtype) for ele in self._interval_steps] 
             cycle_progress = step / total_steps
             cycle = tf.floor(1 + cycle_progress)
-            percentage_complete = 1.0 - tf.abs(cycle - cycle_progress)  # percent of iterations done
-            compare = [percentage_complete <= self.shift_points[0]]
-            accum_shift_points = [self.shift_points[0]]
+            percentage_complete = 1.0 - tf.abs(cycle - cycle_progress)
+            compare = [percentage_complete <= self.interval_fractions[0]]
+            interval_cumul = [self.interval_fractions[0]]
             normalized_steps = [step - (cycle -1)*total_steps]
             masks = [tf.cast(compare[0], dtype)]
 
-            for idx in range(1, len(self.shift_points)):
-                accum_shift_points += [accum_shift_points[idx-1] + self.shift_points[idx]]
-                compare += [percentage_complete <= accum_shift_points[idx]]
+            for idx in range(1, len(self.interval_fractions)):
+                interval_cumul += [interval_cumul[idx-1] + self.interval_fractions[idx]]
+                compare += [percentage_complete <= interval_cumul[idx]]
                 masks += [tf.cast(compare[idx-1] ^ compare[idx], dtype)]
                 normalized_steps += [normalized_steps[idx-1] - tf.squeeze(shift_steps[idx-1])]
             
@@ -85,7 +83,7 @@ class ComposeLR(tf.keras.optimizers.schedules.LearningRateSchedule):
             lr_segments = tf.map_fn(
                 lambda x: self.list_funcs[x](
                     step = normalized_steps[x]/ shift_steps[x]
-                ), tf.range(len(self._shift_steps)),
+                ), range(len(self._interval_steps)),
                 fn_output_signature = dtype
             )
             
@@ -106,47 +104,49 @@ class ComposeLR(tf.keras.optimizers.schedules.LearningRateSchedule):
             "scale_mode": self.scale_mode
         }
     
+    
 def StepWiseLR(cycle_size, 
                initial_learning_rate = 0.1,
                alpha_factor = 0.2,
-               shift_points = [0.2, 0.2, 0.2, 0.2, 0.2],
+               interval_fractions = [0.2, 0.2, 0.2, 0.2, 0.2],
                ):
     
     return ComposeLR(cycle_size = cycle_size,
                initial_learning_rate= initial_learning_rate,
-               shift_points = shift_points,
+               interval_fractions = interval_fractions,
                list_funcs = list(
                     map(lambda x: partial(
                                         constant_func, 
                     learning_rate = initial_learning_rate * alpha_factor ** tf.cast(x, dtype =tf.float32)
-                    ), tf.range(len(shift_points)))
+                    ), tf.range(len(interval_fractions)))
                ),
                name = "StepWise_Constant"
                )
     
+    
 def Goyal_LR(cycle_size, 
                initial_learning_rate = 0.1,
-               shift_points = [0.05, 0.3, 0.3, 0.3],
+               interval_fractions = [0.05, 0.3, 0.3, 0.3],
                warm_up_func = [partial(linear_func, start = 0, end = 0.1)],
                alpha_factor = 0.1,
                cool_down_func = None,
                ):
-    len_shift_points_stepwise = len(shift_points)
+    len_intervals_stepwise = len(interval_fractions)
     if warm_up_func:
-        len_shift_points_stepwise -= 1
+        len_intervals_stepwise -= 1
     
     if cool_down_func:
-        len_shift_points_stepwise -= 1
+        len_intervals_stepwise -= 1
         
     
     return ComposeLR(cycle_size = cycle_size,
                initial_learning_rate= initial_learning_rate,
-               shift_points = shift_points,
+               interval_fractions = interval_fractions,
                list_funcs = warm_up_func + list(
                     map(lambda x: partial(
                                         constant_func, 
                     learning_rate = initial_learning_rate * alpha_factor ** tf.cast(x, dtype =tf.float32)
-                    ), tf.range(len_shift_points_stepwise))
+                    ), tf.range(len_intervals_stepwise))
                ),
                name = "Goyal_LR"
                )    
@@ -155,7 +155,7 @@ def Goyal_LR(cycle_size,
 def Cyclical_LR(cycle_size, 
               initial_learning_rate,
               maximum_learning_rate,
-              shift_points = [0.5, 0.5],
+              interval_fractions = [0.5, 0.5],
               scale_mode = "cycle",
               scale_fn = lambda x:1.0,
               final_lr_scale = 1.0
@@ -165,7 +165,7 @@ def Cyclical_LR(cycle_size,
     return ComposeLR(
         initial_learning_rate = maximum_learning_rate,
         cycle_size = cycle_size,
-        shift_points= shift_points,
+        interval_fractions= interval_fractions,
         list_funcs = [partial(linear_func, start= initial_learning_rate, 
                               end= maximum_learning_rate), 
                       partial(linear_func, start =maximum_learning_rate, 

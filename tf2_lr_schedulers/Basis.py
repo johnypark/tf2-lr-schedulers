c89c858b6cb8c7ec4034687acb9503949dc4c43a
johnypark <parkjohnyc@gmail.com>
2023-03-19 13:46:13 -0400

    Update Basis.py

--- tf2_lr_schedulers/Basis.py
+++ tf2_lr_schedulers/Basis.py
@@ -1,325 +1,75 @@
 #!python
 import numpy as np
 import tensorflow as tf
+from functools import partial
 
+def constant_func(learning_rate, step):
+    output = tf.ones(step.shape)*learning_rate
+    return output
 
-class WarmUp:
-    
-    def __init__(self,
-                 init_LR,
-                 max_LR,
-                 step_size,
-                 scale_mode = 'linear',
-                 scale_fn = None
-                 ):
-        self.iLR = tf.convert_to_tensor(init_LR)
-        self.data_type = self.iLR.dtype
-        self.mLR = tf.cast(max_LR, self.data_type)
-        self.step_size = tf.cast(step_size, self.data_type)
-        self.scale_mode = scale_mode
-        
-    def __call__(self, step):
-        x = tf.cast(step, self.data_type)
-        x = x/(self.step_size)
-        beta = (self.mLR - self.iLR)
-        fx = beta*x
-        return tf.cast(fx, dtype = self.data_type)
-        
-    def get_config(self):
-        return {
-        "initial_learning_rate": self.iLR,
-        "step_size": self.step_size}
-        
-    def total_steps(self):
-        return self.step_size
- 
-class StepDecrease:
-    
-    def __init__(self,
-                 max_LR,
-                 step_size,
-                 change_at, 
-                 scale = 0.1,
-                 cls_dtype = tf.float32
-                 ):
-    
-        self.mLR = max_LR
-        self.step_size = step_size
-        assert type(change_at) is list, "argument should be a list."
-        for point_value in change_at:
-            assert (
-                point_value < step_size
-                ), "chage_at should be smaller than \
-                    the step size ({}=>{})".format(
-                        point_value, 
-                        step_size)
-        self.change_at = change_at
-        self.dtype = cls_dtype
-        self.scale = scale
-        
-    def scale_func(self, idx, scale, mask_list):
-        mask_mat = tf.gather(mask_list, idx)
-        mask_mat = tf.cast(mask_mat, dtype = tf.int32)
-        idx = tf.cast(idx*mask_mat, dtype = self.dtype)
-        return scale**idx
-       
-    def __call__(self, step):
-        step = tf.convert_to_tensor(step)
-        step = tf.cond( tf.rank(step) == 0,
-            lambda: tf.expand_dims(step, axis = 0),
-            lambda: step)
-        compare = list()
-        mask = list()
-        compare += [step < self.change_at[0]]
-        mask += [step < self.change_at[0]]
-        
-        for idx in range(1, len(self.change_at)):
-            compare += [step < self.change_at[idx]]
-            mask += [compare[idx]^compare[idx-1]]
-        compare = [tf.cast(ten, dtype = self.dtype) for ten in compare]
-        mask += [tf.math.add_n(compare)<1]
-        mask_range = tf.range(len(self.change_at)+1)
-        lr_segments = tf.map_fn(
-            fn = lambda idx: self.scale_func(
-                idx = idx, 
-                scale = self.scale, 
-                mask_list = mask),
-            elems = mask_range,
-            fn_output_signature= self.dtype)
-        
-        output = tf.math.reduce_prod(lr_segments, axis = 0)
-        output *= self.mLR
-        return output
-    
-    def get_config(self):
-        return {
-        "inital_learning_rate": self.mLR,
-        "step_size": self.step_size}
-    
-    def total_steps(self):
-        return self.step_size
+def cosine_annealing_func(step, start, end):#, step_size_partial, cycle):
+    x = step # / step_size_partial
+    cosine_annealing = 1 + tf.math.cos(tf.constant(np.pi) * x)
+    return end + 0.5 * (start - end) * cosine_annealing
 
+def linear_func(step, start, end):
+    x = step
+    beta = (end - start)
+    linear = beta * x + start
+    return linear
 
-#https://stackoverflow.com/questions/75323974/performance-difference-between-tf-boolean-mask-and-tf-gather-tf-where
-# tf.where + tf.gather is much better than the tf.boolean_mask
-def apply_funcs2intervals(step, 
-                         list_interval,
-                         list_funcs,
-                         data_type
-                         ):
-    assert (
-        len(list_interval) == len(list_funcs)
-    ), "Number of LR functions and intervals must match."
-    compare = list()
-    mask = list()
-    func_output = list()
-    curr_thres = list()
-    step = tf.cond( tf.rank(step) == 0,
-            lambda: tf.expand_dims(step, axis = 0),
-            lambda: step)
-    
-    curr_thres += [list_interval[0]]
-    condition = step < curr_thres[0]
-    compare += [condition]
-    mask += [condition]
-    where = tf.where(condition)
-    masked_step = tf.gather(step, where)
-    func_output += [list_funcs[0](masked_step)]
-    
-    for idx in range(1, len(list_interval)):
-        curr_thres += [curr_thres[idx-1] + list_interval[idx]]
-        compare += [step < curr_thres[idx]]
-        curr_mask = tf.where(compare[idx] ^ compare[idx - 1])
-        masked_step = tf.gather(step, curr_mask)
-        masked_step = masked_step - curr_thres[idx-1]
-        func_output += [list_funcs[idx](masked_step)]
-        mask += [curr_mask]
-    compare = [tf.cast(ele, dtype = data_type) for ele in compare]
-    final_mask = tf.math.add_n(compare) < 1
-    final_mask = tf.where(final_mask)
-    mask += [final_mask]
-    masked_step = tf.gather(step, final_mask)
-    masked_step = masked_step - curr_thres[idx-1]
-    func_output += [list_funcs[idx](masked_step)]
-      
-    return tf.squeeze(tf.concat(func_output, axis =0))
-  
-  
-class ConnectLRs:
-    
-    def __init__(self, 
-                 list_of_LRs,
-                 max_LR,
-                 list_of_intervals = None):
-        
-        if list_of_intervals:
-            self.step_size = list_of_intervals
-        else:
-            self.step_size = [lr.get_config()["step_size"] for lr in list_of_LRs]
-        self.list_of_LRs = list_of_LRs
-        self.max_LR = max_LR
-        
-    def __call__(self, step):
-        max_LR = tf.convert_to_tensor(self.max_LR)
-        output = apply_funcs2intervals(step, 
-                             list_interval = self.step_size,
-                             list_funcs = self.list_of_LRs,
-                             data_type = max_LR.dtype
-        )
-        return output
-   
-   
-class ConstantLR:
-    def __init__(
-        self, 
-        learning_rate,
-        step_size
-        ):
-        self.learning_rate = learning_rate
-        self.step_size = step_size
-
-    def __call__(self, step):
-        LR = self.learning_rate
-        #step = tf.cast(step, tf.int32)
-        #step_shape = tf.shape(step)
-        constant = tf.ones(shape = step.shape)*LR
-        return constant
-    
-    def get_config(self):
-        return {
-        "learning_rate": self.learning_rate,
-        "step_size": self.step_size}
-               
-class Goyal_LR(tf.keras.optimizers.schedules.LearningRateSchedule):
-    def __init__(self, 
-                 steps_per_epoch, 
-                 init_LR =0.0,
-                 name = 'Goyal'):
-        super(Goyal_LR, self).__init__()
-        
-        max_LR = 0.1
-    
-        self.initial = WarmUp(init_LR = init_LR,
-                max_LR = 0.1,
-                step_size = 5*steps_per_epoch)
-        
-        self.constant1 = ConstantLR(learning_rate = 0.1,
-                                    step_size = 30*steps_per_epoch)
-        
-        self.constant2 = ConstantLR(learning_rate = 0.1**2,
-                                    step_size = 30*steps_per_epoch)
-        
-        
-        self.constant3 = ConstantLR(learning_rate = 0.1**3,
-                                    step_size = 30*steps_per_epoch)
-        
-        
-        self.constant4 = ConstantLR(learning_rate = 0.1**4,
-                                    step_size = 10*steps_per_epoch)
-        
-        self.lr_scheduler = ConnectLRs([self.initial, 
-                                        self.constant1,
-                                        self.constant2,
-                                        self.constant3,
-                                        self.constant4],
-                                        max_LR = max_LR)
-        self.name = name
-
-    def __call__(self, step, optimizer = False):
-      with tf.name_scope(self.name):
-        return self.lr_scheduler(step)
-    
-class Goyal_style_LR(tf.keras.optimizers.schedules.LearningRateSchedule):
-    def __init__(self, 
-                 max_LR, 
-                 rate_decrease,
-                 steps_per_epoch, 
-                 intervals = [5, 30, 30, 30, 10], 
-                 init_LR = 0.0,
-                 
-                 
-                 name = 'Goyal_style'):
-        super(Goyal_style_LR, self).__init__()
-    
-        self.initial = WarmUp(init_LR = init_LR,
-                max_LR = max_LR,
-                step_size = intervals[0]*steps_per_epoch)
-        
-        self.constant1 = ConstantLR(learning_rate = max_LR,
-                                    step_size = intervals[1]*steps_per_epoch)
-        
-        self.constant2 = ConstantLR(learning_rate = max_LR * rate_decrease,
-                                    step_size = intervals[2]*steps_per_epoch)
-        
-        
-        self.constant3 = ConstantLR(learning_rate = max_LR * rate_decrease **2,
-                                    step_size = intervals[3]*steps_per_epoch)
-        
-        
-        self.constant4 = ConstantLR(learning_rate = max_LR *rate_decrease **3,
-                                    step_size = intervals[4]*steps_per_epoch)
-        
-        self.lr_scheduler = ConnectLRs([self.initial, 
-                                        self.constant1,
-                                        self.constant2,
-                                        self.constant3,
-                                        self.constant4],
-                                       max_LR = max_LR)
-        self.name = name
-
-    def __call__(self, step, optimizer = False):
-      with tf.name_scope(self.name):
-        return self.lr_scheduler(step)
-    
-
-class StepWiseLR(tf.keras.optimizers.schedules.LearningRateSchedule):
+class ComposeLR(tf.keras.optimizers.schedules.LearningRateSchedule):
 
     def __init__(
         self,
         cycle_size,
+        shift_points, 
+        list_funcs, 
         initial_learning_rate = 0.1,
-        alpha_factor = 0.2,
-        scale_fn=lambda x: 1.0,
-        shift_points=[0.2, 0.2, 0.2, 0.2, 0.05],
         scale_mode="cycle",
         final_lr_scale=1.0,
-        name=None,
-    ):
+        scale_fn=lambda x: 1.0,
+        name="ComposeLR",
+    ):  
+        """_summary_
+
+        Args:
+            cycle_size (_type_): _description_
+            shift_points (list): # list of shifting points, check if sum(shift_points)< =1, otherwise need rescaling. 
+            list_funcs (list), # list of functions
+            initial_learning_rate (float, optional): _description_. Defaults to 0.1.
+            final_lr_scale (float, optional): _description_. Defaults to 1.0.
+            scale_fn (_type_, optional): _description_. Defaults to lambdax:1.0.
+            name (str, optional): _description_. Defaults to "ComposeLR".
+        """
         super().__init__()
         self.initial_learning_rate = initial_learning_rate
-        self.alpha_factor = alpha_factor
         self.cycle_size = cycle_size
         self.scale_fn = scale_fn
         self.scale_mode = scale_mode
         self.shift_points = shift_points
         self.final_lr_scale = final_lr_scale
         self.name = name
-        # Defines the position of the max lr in steps
         self._total_steps = cycle_size
         self._shift_steps = [ele*self._total_steps for ele in self.shift_points]
-
-    def get_constant(self, learning_rate, step, step_size_partial, cycle):
-        x = step / step_size_partial
-        x.shape
-        output = tf.ones(x.shape)*learning_rate
-        return output
+        self.list_funcs = list_funcs
 
     def __call__(self, step, optimizer=False):
-        with tf.name_scope(self.name or "OneCycle"):
+        with tf.name_scope(self.name or "ComposeLR"):
             initial_learning_rate = tf.convert_to_tensor(self.initial_learning_rate, name="initial_learning_rate")
             
             dtype = initial_learning_rate.dtype
             step = tf.cast(step, dtype)
-            alpha_factor = tf.cast(self.alpha_factor, dtype)
+            step = tf.cond( tf.rank(step) == 0,
+            lambda: tf.expand_dims(step, axis = 0),
+            lambda: step)
             total_steps = tf.cast(self._total_steps, dtype)
             print(self._shift_steps)
             shift_steps = [tf.cast(ele, dtype) for ele in self._shift_steps] 
-            # Check in % the cycle
+            # Check in progress on the cycle
             cycle_progress = step / total_steps
             cycle = tf.floor(1 + cycle_progress)
-
             percentage_complete = 1.0 - tf.abs(cycle - cycle_progress)  # percent of iterations done
-            
             compare = [percentage_complete <= self.shift_points[0]]
             accum_shift_points = [self.shift_points[0]]
             normalized_steps = [step - (cycle -1)*total_steps]
@@ -329,30 +79,19 @@ class StepWiseLR(tf.keras.optimizers.schedules.LearningRateSchedule):
                 accum_shift_points += [accum_shift_points[idx-1] + self.shift_points[idx]]
                 compare += [percentage_complete <= accum_shift_points[idx]]
                 masks += [tf.cast(compare[idx-1] ^ compare[idx], dtype)]
-                #print("Normalized steps {}".format(normalized_steps))
-                #print("Shift_steps {}".format(shift_steps[idx-1]))
                 normalized_steps += [normalized_steps[idx-1] - tf.squeeze(shift_steps[idx-1])]
             
-            masks = tf.stack(masks, axis = 0)
-            #print("len Normalized steps: {}".format(len(normalized_steps)))
-            #print(shift_steps)
+            masks = tf.stack(masks, axis = 0)         
             lr_segments = tf.map_fn(
-                lambda x: self.get_constant(
-                    initial_learning_rate * alpha_factor ** tf.cast(x, dtype),
-                    normalized_steps[x],
-                    shift_steps[x],
-                    cycle
+                lambda x: self.list_funcs[x](
+                    step = normalized_steps[x]/ shift_steps[x]
                 ), tf.range(len(self._shift_steps)),
                 fn_output_signature = dtype
             )
-            print(lr_segments)
-            #print(masks)
+            
             lr_res = masks*lr_segments
-
-            print(lr_res)
             lr_res = tf.math.reduce_sum(lr_res, axis = 0)
       
-
             mode_step = cycle if self.scale_mode == "cycle" else step
 
             if optimizer == False:
@@ -364,9 +103,77 @@ class StepWiseLR(tf.keras.optimizers.schedules.LearningRateSchedule):
         return {
             "initial_learning_rate": self.initial_learning_rate,
             "cycle_size": self.cycle_size,
-            "scale_mode": self.scale_mode,
-            "shift_peak": self.shift_peak,
-            "final_lr_scale": self.final_lr_scale
+            "scale_mode": self.scale_mode
         }
     
+def StepWiseLR(cycle_size, 
+               initial_learning_rate = 0.1,
+               alpha_factor = 0.2,
+               shift_points = [0.2, 0.2, 0.2, 0.2, 0.2],
+               ):
+    
+    return ComposeLR(cycle_size = cycle_size,
+               initial_learning_rate= initial_learning_rate,
+               shift_points = shift_points,
+               list_funcs = list(
+                    map(lambda x: partial(
+                                        constant_func, 
+                    learning_rate = initial_learning_rate * alpha_factor ** tf.cast(x, dtype =tf.float32)
+                    ), tf.range(len(shift_points)))
+               ),
+               name = "StepWise_Constant"
+               )
+    
+def Goyal_LR(cycle_size, 
+               initial_learning_rate = 0.1,
+               shift_points = [0.05, 0.3, 0.3, 0.3],
+               warm_up_func = [partial(linear_func, start = 0, end = 0.1)],
+               alpha_factor = 0.1,
+               cool_down_func = None,
+               ):
+    len_shift_points_stepwise = len(shift_points)
+    if warm_up_func:
+        len_shift_points_stepwise -= 1
+    
+    if cool_down_func:
+        len_shift_points_stepwise -= 1
+        
+    
+    return ComposeLR(cycle_size = cycle_size,
+               initial_learning_rate= initial_learning_rate,
+               shift_points = shift_points,
+               list_funcs = warm_up_func + list(
+                    map(lambda x: partial(
+                                        constant_func, 
+                    learning_rate = initial_learning_rate * alpha_factor ** tf.cast(x, dtype =tf.float32)
+                    ), tf.range(len_shift_points_stepwise))
+               ),
+               name = "Goyal_LR"
+               )    
+    
+        
+def Cyclical_LR(cycle_size, 
+              initial_learning_rate,
+              maximum_learning_rate,
+              shift_points = [0.5, 0.5],
+              scale_mode = "cycle",
+              scale_fn = lambda x:1.0,
+              final_lr_scale = 1.0
+              ):
+    
+    
+    return ComposeLR(
+        initial_learning_rate = maximum_learning_rate,
+        cycle_size = cycle_size,
+        shift_points= shift_points,
+        list_funcs = [partial(linear_func, start= initial_learning_rate, 
+                              end= maximum_learning_rate), 
+                      partial(linear_func, start =maximum_learning_rate, 
+                              end=initial_learning_rate)],
+        scale_mode= scale_mode,
+        scale_fn = scale_fn,
+        final_lr_scale= final_lr_scale,
+        name= 'Triangluar_Cyclical_LR',
+    )
+
     
